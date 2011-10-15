1834a64051cdc942227b12184d3412e6492ebde0
Jxck <block.rxckin.beats@gmail.com>
2011-10-15 17:55:37 +0900

    add spec to text2patch

--- spec/diff_launch.test.js
+++ spec/diff_launch.test.js
@@ -1,10 +1,14 @@
 if (typeof window === 'undefined') {
   // for jasmine-node
-  var diff_launch = require('../lib/diff_launch');
+  var text2patch = require('../lib/diff_launch');
 }
 
 describe('jasmine-node', function() {
   it('should pass', function() {
-    expect(diff_launch()).toEqual('ok');
+    expect(text2patch).toBeTruthy();
+  });
+  it('should return patch_text', function() {
+    var patch = text2patch('asdf', 'adf');
+    expect(patch).toBeTruthy();
   });
 });
