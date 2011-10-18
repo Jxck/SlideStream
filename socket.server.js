8e88563d016478ca01a7fcc397e83877078dfa3a
Jxck <block.rxckin.beats@gmail.com>
2011-10-18 23:30:46 +0900

    add production configure for socket.io

--- socket.server.js
+++ socket.server.js
@@ -37,15 +37,14 @@ io.configure(function() {
 
 io.configure('production', function() {
   // io.enable('browser client etag');
-  // io.set('log level', 1);
-
-  // io.set('transports', [
-  //   'websocket'
-  // , 'flashsocket'
-  // , 'htmlfile'
-  // , 'xhr-polling'
-  // , 'jsonp-polling'
-  // ]);
+  io.set('log level', 1);
+  io.set('transports', [
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+  ]);
 });
 
 
