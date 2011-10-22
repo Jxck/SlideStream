660aca493d24897a3380874db23d03e3411ade9a
Jxck <block.rxckin.beats@gmail.com>
2011-10-22 17:08:36 +0900

    refuctoring

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,16 +1,37 @@
 var log = console.log.bind(console);
 // Client
+var socket = io.connect();
+
 var resultCache = [];
 
-var socket = io.connect();
+function codeRender(target, data) {
+  var $target = $('#' + target);
+  $target.text(data);
+  if (target === 'code') sh_highlightDocument('lang/', '.js');
+  if (target === 'result') sh_highlightDocument('lang/', '.shell');
+}
+
+var codeCache = '';
+function buildResult(target, patch) {
+  var old_text = codeCache;
+  var result = patch !== 'empty'? apply_patch(old_text, patch) : '';
+  codeCache = result;
+  codeRender(target, result);
+log('result', patch.length);
+}
+
+function insertEmpty(target, html) {
+  var $target = $('section.' + target);
+  $target.html(html);
+}
 
 socket.on('connect', function() {
   log('connect');
+
   // move slide
   socket.on('go', function(to) {
     $.deck('go', to);
   });
-
   $(document).bind('deck.change', function(event, from, to) {
     if (from != to) socket.emit('go', to);
   });
@@ -44,24 +65,3 @@ $(function() {
   $('pre.sh_javascript, pre.sh_sh').height(section_height - h2_height * 2);
   $('pre.full').height(section_height - h2_height / 2);
 });
-
-function codeRender(target, data) {
-  var $target = $('#' + target);
-  $target.text(data);
-  if (target === 'code') sh_highlightDocument('lang/', '.js');
-  if (target === 'result') sh_highlightDocument('lang/', '.shell');
-}
-
-var codeCache = '';
-function buildResult(target, patch) {
-  var old_text = codeCache;
-  var result = apply_patch(old_text, patch);
-  codeCache = result;
-  codeRender(target, result);
-log('result', patch.length);
-}
-
-function insertEmpty(target, html) {
-  var $target = $('section.' + target);
-  $target.html(html);
-}
