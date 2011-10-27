907425f583dbc79d22fe45eb169e38227703868b
Jxck <block.rxckin.beats@gmail.com>
2011-10-27 16:09:52 +0900

    update slide iframe

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -144,7 +144,8 @@ socket.on('connect', function() {
 $(function() {
   var section_height = $('section').height();
   var h2_height = $('h2').height();
-  $('iframe').height(section_height);
+  var h3_height = $('h3').height();
+  $('iframe').height(section_height - h3_height);
   $('pre.sh_javascript, pre.sh_sh').height(section_height - h2_height * 2);
   $('pre.half').height(section_height / 2.7);
   $('pre.full').height(section_height - h2_height / 2);
